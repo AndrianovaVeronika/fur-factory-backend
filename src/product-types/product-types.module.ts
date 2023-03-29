713a5e3016fb7af97bc77a8147962daa2254e399
andrianova.v20 <andrianova.v20@cf.dnu.dp.ua>
2023-03-29 20:03:50 +0200

    add: ProductTypes controller initial

--- src/product-types/product-types.module.ts
+++ src/product-types/product-types.module.ts
@@ -1,5 +1,8 @@
 import {Module} from '@nestjs/common'
+import { ProductTypesController } from './product-types.controller';
 
-@Module({})
+@Module({
+  controllers: [ProductTypesController]
+})
 export class ProductTypesModule {
 }
